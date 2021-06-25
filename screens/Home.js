970060bbed393b95fe749c3cfec1c7b8dd91d38e
esteloabell <manitobaaa@gmail.com>
2021-06-25 11:34:35 -0500

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,20 +1,205 @@
 import React from 'react';
 import {
+  SafeAreaView,
   View,
   Text,
-  TouchableOpacity
+  FlatList,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Image,
+  ImageBackground,
+  Animated,
+  ScrollView
 } from 'react-native';
 
+import {Profiles} from '../components';
+
+import { dummyData, COLORS, SIZES, FONTS, icons, images } from '../constants';
+
 const Home = ({ navigation }) => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home</Text>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("MovieDetail")}
+
+  const newSeasonScrollX = React.useRef(new Animated.Value(0)).current;
+
+  function renderHeader() {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          paddingHorizontal: SIZES.padding,
+          marginTop: 30,
+        }}
       >
-        <Text>Navigate to MovieDetail</Text>
-      </TouchableOpacity>
-    </View>
+        {/* Profile */}
+        <TouchableOpacity
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: 50,
+            height: 50
+          }}
+          onPress={() => console.log('Profile')}
+        >
+          <Image 
+            source={icons.project}
+            style={{
+              width: 40,
+              height: 40,
+              borderRadius: 20,
+            }}
+          />
+        </TouchableOpacity>
+
+        {/* Screen Mirror */}
+        <TouchableOpacity
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 50,
+            height: 50,
+          }}
+          onPress={() => console.log('Screen Mirror')}
+        >
+          <Image source={icons.airplay} 
+            style={{
+              width: 25,
+              height: 25,
+              tintColor: COLORS.primary
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  function renderNewSeasonSection() {
+    return (
+      <Animated.FlatList 
+        horizontal
+        pagingEnabled
+        snapToAlignment="center"
+        snapToInterval={SIZES.width}
+        showsHorizontalScrollIndicator={false}
+        scrollEventThrottle={16}
+        decelerationRate={0}
+        contentContainerStyle={{
+          marginTop: SIZES.radius
+        }}
+        data={dummyData.newSeason}
+        keyExtractor={item => `${item.id}`}
+        onScroll={Animated.event([
+          { nativeEvent: { contentOffset: { x: newSeasonScrollX }} }
+        ], { useNativeDriver: false })}
+        renderItem={({item, index}) => {
+          return (
+            <TouchableWithoutFeedback
+              onPress={() => navigation.navigate('MovieDetail', { selectedMovie: item })}
+            >
+              <View 
+                style={{
+                  width: SIZES.width,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <ImageBackground
+                  source={item.thumbnail}
+                  resizeMode="cover"
+                  style={{
+                    width: SIZES.width * 0.85,
+                    height: SIZES.height * 0.35,
+                    justifyContent: 'flex-end'
+                  }}
+                  imageStyle={{
+                    borderRadius: 40
+                  }}
+                >
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      height: 30,
+                      width: '100%',
+                      marginBottom: SIZES.radius,
+                      paddingHorizontal: SIZES.radius,
+                    }}
+                  >
+                    {/* Play Now */}
+                    <View
+                      style={{
+                        flex: 1,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <View
+                        style={{
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                          width: 40,
+                          height: 40,
+                          borderRadius: 20,
+                          backgroundColor: COLORS.transparentWhite
+                        }}
+                      >
+                        <Image 
+                          source={icons.play}
+                          resizeMode="contain"
+                          style={{
+                            width: 15,
+                            height: 15,
+                            tintColor: COLORS.white
+                          }}
+                        />
+                      </View>
+
+                      <Text style={{
+                        marginLeft: SIZES.base, 
+                        color: COLORS.white, 
+                        ...FONTS.h3
+                      }}>Play Now</Text>
+                    </View>
+                    {/* Still Watching */}
+                    {item.stillWatching.length > 0 &&
+                      <View style={{
+                        justifyContent: 'center'
+                      }}>
+                        <Text
+                          style={{
+                            color: COLORS.white,
+                            ...FONTS.h4
+                          }}
+                        >Still Watching</Text>
+                        <Profiles />
+                      </View>
+                    }
+                  </View>
+                </ImageBackground>
+              </View>
+            </TouchableWithoutFeedback>
+          )
+        }}
+      />
+    )
+  }
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.black,
+      }}
+    >
+      {renderHeader()}
+
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 100
+        }}
+      >
+        {renderNewSeasonSection()}
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
