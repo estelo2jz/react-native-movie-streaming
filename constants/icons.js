7e1b4ff9e4754def40c347b920d478abbc16ad29
esteloabell <manitobaaa@gmail.com>
2021-06-25 11:34:31 -0500

    Update icons.js

--- constants/icons.js
+++ constants/icons.js
@@ -4,6 +4,7 @@ const left_arrow = require("../assets/icons/left-arrow.png");
 const play = require("../assets/icons/play.png");
 const play_button = require("../assets/icons/play-button.png");
 const profile = require("../assets/icons/profile.png");
+const project = require("../assets/icons/project64.png");
 const right_arrow = require("../assets/icons/right-arrow.png");
 const search = require("../assets/icons/search.png");
 const star = require("../assets/icons/star.png");
@@ -16,6 +17,7 @@ export default {
   play,
   play_button,
   profile,
+  project,
   right_arrow,
   search,
   star,
